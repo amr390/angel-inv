5cf24881d7e06a076cbee63beb4db2c63b41dfed
amr <dquijote22@gmail.com>
2025-02-28 13:46:17 +0100

    [dev] startup catalog

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -22,6 +22,11 @@ export default async function Home({
 
         <SearchForm query={query} />
       </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for ${query}` : "All startups"}
+        </p>
+      </section>
     </>
   );
 }
