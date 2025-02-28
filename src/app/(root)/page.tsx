46b6f1391a5f29713d45500275409efaffb55607
amr <dquijote22@gmail.com>
2025-02-28 13:38:25 +0100

    [conf] initial commit + landing hero

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { SearchForm } from "@/components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch your Startup, <br /> Conect with Entrepeneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          competitions.
+        </p>
+
+        <SearchForm query={query} />
+      </section>
+    </>
+  );
+}
